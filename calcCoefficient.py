a61337760dcbde5b7f050ad15f85d32a515928a3
angel-star <angel-stars@qq.com>
2016-11-25 17:00:35 +0800

    Update calcCoefficient.py

--- calcCoefficient.py
+++ calcCoefficient.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 def calcCoefficient(data, listA, listW, listLostFunction):
-    N = len(data[0])
+    N = len(data[0]) #维度
     w = [0 for i in range(N)]
     wNew = [0 for i in range(N)]
     g = [0 for i in range(N)]
 
     times = 0
-    alpha = 100.0
+    alpha = 100.0 # 学习率随意初始化
     while times < 10000:
         j = 0
         while j < N :
             g[j] = gradient(data, w, j)
             j += 1
-        normalize(g)
+        normalize(g) # 正则化梯度
         alpha = calcAlpha(w, g, alpha, data)
         numberProduct(alpha, g, wNew)
 
         print "times,alpha,fw,w,g:\t", times, alpha, fw(w,data), w, g
         if isSame(w, wNew):
             break
-        assign2(w, wNew)
+        assign2(w, wNew) # 更新权值
         times += 1
 
         listA.append(alpha)
